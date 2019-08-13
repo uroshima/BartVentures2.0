bac88a07039f76b27dc23b50d1ab2f9b2e723cc6
urosh <urosh.stojkovikj@gmail.com>
2019-08-13 10:45:15 -0700

    fixed the logout button

--- frontend/src/components/navBar/nav_bar.jsx
+++ frontend/src/components/navBar/nav_bar.jsx
@@ -23,9 +23,11 @@ class NavBar extends React.Component {
         {/* <span className='attractions'>to see the attractions nearby</span> */}
         </div>;
     } else {
+      let firstName = this.props.currentUser.split(' ');
+      firstName = firstName[0];
       return (
-        <div className='greeting'>
-          <h1 className='current-user' >Hello, {this.props.currentUser}</h1>
+        <div className='sessionLinks'>
+          <h1 className='current-user' >{firstName}</h1>
             <button className='logout-button' onClick= {this.props.logoutUser}>Log out</button>
         </div>
       );
