a2c292bf2af4fa7683f5fdcc98900ec4aa64bb41
urosh <urosh.stojkovikj@gmail.com>
2019-08-20 13:54:30 -0700

    changed the keys

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,10 @@
+// module.exports = {
+//   mongoURI: process.env.MONGO_URI,
+//   secretOrKey: process.env.SECRET_OR_KEY
+// }
+
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  secretOrKey: process.env.SECRET_OR_KEY
-}
+  mongoURI: 'mongodb://uroshima:ludnicaM1@ds133252.mlab.com:33252/uroshima',
+    //Make sure this is your own unique string
+  secretOrKey: "secret"
+};
